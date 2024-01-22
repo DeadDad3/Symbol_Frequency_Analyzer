e44b898827398616c6a37e85e29f3a9066a69890
DeadDad3 <deaddad3@mail.ru>
2024-01-22 12:28:21 +0400

    Created a Spring project.

---
+++ Symbol_Frequency_Analyzer/src/main/java/com/example/symbol_frequency_analyzer/SymbolFrequencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.symbol_frequency_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolFrequencyAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymbolFrequencyAnalyzerApplication.class, args);
+    }
+
+}
---
+++ Symbol_Frequency_Analyzer/src/test/java/com/example/symbol_frequency_analyzer/SymbolFrequencyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.symbol_frequency_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbolFrequencyAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
