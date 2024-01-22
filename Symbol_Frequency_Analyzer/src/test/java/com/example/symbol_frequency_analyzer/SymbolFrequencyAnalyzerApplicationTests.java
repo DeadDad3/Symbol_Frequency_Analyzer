3d2c92d19c5d4e57a0c4973cb65872c6c06fab17
DeadDad3 <deaddad3@mail.ru>
2024-01-22 18:15:52 +0400

    Made some new variations of tests

--- Symbol_Frequency_Analyzer/src/test/java/com/example/symbol_frequency_analyzer/SymbolFrequencyAnalyzerApplicationTests.java
+++ Symbol_Frequency_Analyzer/src/test/java/com/example/symbol_frequency_analyzer/SymbolFrequencyAnalyzerApplicationTests.java
@@ -46,6 +46,93 @@ public class SymbolFrequencyAnalyzerApplicationTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
     }
 
-    // Добавить дополнительные тесты для проверки других сценариев
+    @Test
+    public void testCalculateFrequency_differentCharacters() throws Exception {
+        String inputString = "abcde";
+        Map<Character, Integer> expectedFrequencyMap = new LinkedHashMap<>();
+        expectedFrequencyMap.put('a', 1);
+        expectedFrequencyMap.put('b', 1);
+        expectedFrequencyMap.put('c', 1);
+        expectedFrequencyMap.put('d', 1);
+        expectedFrequencyMap.put('e', 1);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculate-frequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inputString\":\"" + inputString + "\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
+    }
+
+    @Test
+    public void testCalculateFrequency_caseInsensitive() throws Exception {
+        String inputString = "AaBbCc";
+        Map<Character, Integer> expectedFrequencyMap = new LinkedHashMap<>();
+        expectedFrequencyMap.put('a', 2);
+        expectedFrequencyMap.put('b', 2);
+        expectedFrequencyMap.put('c', 2);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculate-frequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inputString\":\"" + inputString + "\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
+    }
+
+    @Test
+    public void testCalculateFrequency_multipleOccurrences() throws Exception {
+        String inputString = "aaabbbccc";
+        Map<Character, Integer> expectedFrequencyMap = new LinkedHashMap<>();
+        expectedFrequencyMap.put('a', 3);
+        expectedFrequencyMap.put('b', 3);
+        expectedFrequencyMap.put('c', 3);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculate-frequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inputString\":\"" + inputString + "\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
+    }
+
+    @Test
+    public void testCalculateFrequency_specialCharacters() throws Exception {
+        String inputString = "!@#$%^&*()";
+        Map<Character, Integer> expectedFrequencyMap = new LinkedHashMap<>();
+        expectedFrequencyMap.put('!', 1);
+        expectedFrequencyMap.put('@', 1);
+        expectedFrequencyMap.put('#', 1);
+        expectedFrequencyMap.put('$', 1);
+        expectedFrequencyMap.put('%', 1);
+        expectedFrequencyMap.put('^', 1);
+        expectedFrequencyMap.put('&', 1);
+        expectedFrequencyMap.put('*', 1);
+        expectedFrequencyMap.put('(', 1);
+        expectedFrequencyMap.put(')', 1);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculate-frequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inputString\":\"" + inputString + "\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
+    }
+
+    @Test
+    public void testCalculateFrequency_whitespaceCharacters() throws Exception {
+        String inputString = "Hello World";
+        Map<Character, Integer> expectedFrequencyMap = new LinkedHashMap<>();
+        expectedFrequencyMap.put('H', 1);
+        expectedFrequencyMap.put('e', 1);
+        expectedFrequencyMap.put('l', 3);
+        expectedFrequencyMap.put('o', 2);
+        expectedFrequencyMap.put(' ', 1);
+        expectedFrequencyMap.put('W', 1);
+        expectedFrequencyMap.put('r', 1);
+        expectedFrequencyMap.put('d', 1);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculate-frequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"inputString\":\"" + inputString + "\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.frequencyMap").value(expectedFrequencyMap));
+    }
 
 }
